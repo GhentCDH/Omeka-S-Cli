bc711f163070e8c0f11c73a9007c7359c3356348
Frederic Lamsens <frederic.lamsens@ugent.be>
2025-04-18 12:36:52 +0200

    add ZipDownloader

---
+++ src/Downloader/ZipDownloader.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace OSC\Downloader;
+
+use Exception;
+use OSC\Helper\FileUtils;
+use ZipArchive;
+
+class ZipDownloader implements DownloaderInterface {
+    public function download(string $url): string
+    {
+        $tmpZipDestinationPath = FileUtils::createTempFolder('omeka-s-cli.');
+        $tmpZipFilePath = FileUtils::createTempFile('omeka-s-cli.');
+
+        try {
+            $tmpZipResource = fopen($tmpZipFilePath, "w+");
+
+            if (!flock($tmpZipResource, LOCK_EX)) {
+                throw new Exception('Failed to lock temporary file');
+            }
+
+            if (!fwrite($tmpZipResource, file_get_contents($url))) {
+                throw new Exception("Failed to download file at '$url'");
+            }
+
+            $zip = new ZipArchive;
+            if (true !== $zip->open($tmpZipFilePath)) {
+                throw new Exception("Failed to open file '$tmpZipFilePath'");
+            }
+
+            if (!$zip->extractTo($tmpZipDestinationPath)) {
+                $zip->close();
+                throw new Exception("Could not unzip file '$tmpZipFilePath'");
+            }
+
+            $zip->close();
+
+            return $tmpZipDestinationPath;
+        }
+        finally {
+            flock($tmpZipResource, LOCK_UN);
+            unlink($tmpZipFilePath);
+        }
+    }
+}
