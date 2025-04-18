8e37d89eb948b0d9e4adb0387a314473d8d3c7ea
Frederic Lamsens <frederic.lamsens@ugent.be>
2025-04-18 12:37:26 +0200

    refactor theme/module api

--- src/Omeka/ThemeApi.php
+++ src/Omeka/ThemeApi.php
@@ -7,7 +7,7 @@ use Omeka\Entity\Site;
 use Omeka\Service\ThemeManagerFactory;
 use Omeka\Site\Theme\Manager as ThemeManager;
 use Omeka\Site\Theme\Theme;
-use Throwable;
+use OSC\Helper\FileUtils;
 
 class ThemeApi
 {
@@ -41,11 +41,22 @@ class ThemeApi
         return $this->themeManager->getThemes();
     }
 
-    public function getTheme(string $theme_id, bool $reload = false): ?Theme
+    public function getTheme(string $themeId, bool $reload = false): Theme
     {
+        // reload themes?
         $reload && $this->reloadThemeManager();
+
+        // find theme
         $themes = $this->getThemes();
-        return $themes[$theme_id] ?? null;
+        foreach($themes as $tmpThemeId => $theme) {
+            $themes[strtolower($tmpThemeId)] = $theme;
+        }
+
+        $theme = $themes[strtolower($themeId)] ?? null;
+        if (!$theme) {
+            throw new Exception("Theme '{$themeId}' not found");
+        }
+        return $theme;
     }
 
     public function isInstalled(string $theme_id): bool
@@ -72,18 +83,14 @@ class ThemeApi
 
     public function delete(Theme $theme): void
     {
-        try {
-            if ($theme->getState() === ThemeManager::STATE_NOT_FOUND) {
-                throw new Exception('The theme can\'t be removed because its source files can not be found on disc.');
-            }
-
-            $path = $theme->getPath();
-            if (empty($path) || $path == '/' || !(str_contains($path, 'themes')))
-                throw new Exception('Incorrect or dangerous path detected. Please remove the folder manually.');
-            system("rm -rf " . escapeshellarg($path));
-        } catch (Throwable $e) {
-            throw new Exception("Could not delete theme: \n" . $e->getMessage());
+        if ($theme->getState() === ThemeManager::STATE_NOT_FOUND) {
+            throw new Exception('The theme can\'t be removed because its source files can not be found on disc.');
         }
+
+        $path = $theme->getPath();
+        if (empty($path) || $path == '/' || !(str_contains($path, 'themes')))
+            throw new Exception('Incorrect or dangerous path detected. Please remove the folder manually.');
+        FileUtils::removeFolder($path);
     }
 
     public function isActiveOnSite(Theme $theme): bool
@@ -92,7 +99,7 @@ class ThemeApi
         /** @var Site[] $sites */
         $sites = $api->search('sites', [], ['responseContent' => 'resource'])->getContent();
         foreach($sites as $site) {
-            if ($site->getTheme() === $theme->getIni()) {
+            if ($site->getTheme() === $theme->getId()) {
                 return true;
             }
         }
