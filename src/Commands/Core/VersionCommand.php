5815acf01f421320c02db9ceb285271b609f1bec
Frederic Lamsens <frederic.lamsens@ugent.be>
2025-09-09 15:36:53 +0200

    fix: typo

--- src/Commands/Core/VersionCommand.php
+++ src/Commands/Core/VersionCommand.php
@@ -15,7 +15,7 @@ class VersionCommand extends AbstractModuleCommand
 
     public function __construct()
     {
-        parent::__construct('core:version', 'Get the current download Omeka S version');
+        parent::__construct('core:version', 'Get the downloaded Omeka S version');
         $this->option('-i --installed', 'Get the installed Omeka S version.', 'boolval', false);
         $this->optionJson();
     }
