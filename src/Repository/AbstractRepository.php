262bc62017d625c18bc23c9b184bc49b58464af7
Frederic Lamsens <frederic.lamsens@ugent.be>
2026-01-19 17:47:49 +0100

    refactor: move repository implementation to abstract class

--- src/Repository/AbstractRepository.php
+++ src/Repository/AbstractRepository.php
@@ -1,12 +1,61 @@
 <?php
 namespace OSC\Repository;
 
+use OSC\Cache;
+use OSC\Cache\CacheInterface;
+
 /**
  * @template T
  */
 abstract class AbstractRepository implements RepositoryInterface
 {
-    abstract public function list(): array;
+    private CacheInterface $cache;
+    private ?string $cacheKey = null;
+
+    public function __construct()
+    {
+        $this->cache = Cache::getCache();
+    }
+
+    abstract protected function entries(): array;
+
+    public function list(): array
+    {
+        $cacheKey = $this->getCacheKey();
+        $entries = $this->cache->get($cacheKey);
+
+        if ( !$entries ) {
+            $entries = $this->entries();
+            $this->cache->set($cacheKey, $entries);
+        }
+        return $entries;
+    }
+
+    /**
+     * Generate a PSR-16 compliant cache key based on the full class name.
+     * Caches the result for performance.
+     *
+     * PSR-16 reserves characters: {}()/\@:
+     *
+     * @return string Safe cache key
+     */
+    protected function getCacheKey(): string
+    {
+        if ($this->cacheKey === null) {
+            // Get full class name with namespace
+            $className = static::class;
+
+            // Remove PSR-16 reserved characters: {}()/\@:
+            // Replace with underscores for readability
+            $this->cacheKey = str_replace(
+                ['\\', '/', '{', '}', '(', ')', '@', ':'],
+                '_',
+                $className
+            );
+        }
+
+        return $this->cacheKey;
+    }
 
     /**
      * @param string $id
@@ -35,5 +84,11 @@ abstract class AbstractRepository implements RepositoryInterface
         return array_filter($this->list(), function ($item) use ($query) {
             return $item->matches($query);
         });
+    }
+
+    public function refresh(): void
+    {
+        $cacheKey = $this->getCacheKey();
+        $this->cache->delete($cacheKey);
     }
 }
