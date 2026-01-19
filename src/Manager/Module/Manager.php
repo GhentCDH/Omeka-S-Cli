75a6f25399dda99eb9bec01c69284c874de07427
Frederic Lamsens <frederic.lamsens@ugent.be>
2026-01-19 11:18:21 +0100

    refactor: move repository initialisation to repository managers

--- src/Manager/Module/Manager.php
+++ src/Manager/Module/Manager.php
@@ -2,11 +2,25 @@
 namespace OSC\Manager\Module;
 
 use OSC\Manager\AbstractManager;
+use OSC\Repository\Module\DanielKM;
 use OSC\Repository\Module\ModuleDetails;
+use OSC\Repository\Module\OmekaDotOrg;
 
 /**
   * @extends AbstractManager<ModuleDetails>
  */
 class Manager extends AbstractManager
 {
+    protected function registerRepositories(): void
+    {
+        // init module repositories
+        $repositories = [
+            new OmekaDotOrg(),
+            new DanielKM(),
+        ];
+
+        foreach ($repositories as $repository) {
+            $this->addRepository($repository);
+        }
+    }
 }
