ffcd09607faf4b2855dfc41010783e7fc6fc466d
Frederic Lamsens <frederic.lamsens@ugent.be>
2026-03-09 15:42:17 +0100

    refactor: improved vocabulary:import-from-repo command usage

--- src/Commands/Vocabulary/ImportFromRepoCommand.php
+++ src/Commands/Vocabulary/ImportFromRepoCommand.php
@@ -15,16 +15,15 @@ class ImportFromRepoCommand extends AbstractCommand
         parent::__construct('vocabulary:import-from-repo', 'Import a vocabulary from a repository');
 
         $this
-            ->argument('<identifier>', 'Vocabulary identifier (ID, prefix, or namespace URI)')
-            ->option('-p --prefix', 'Treat identifier as vocabulary prefix', 'boolval', false)
-            ->option('-n --namespace-uri', 'Treat identifier as vocabulary namespace uri', 'boolval', false)
+            ->argument('<identifier>', 'Vocabulary identifier')
+            ->option('-p --prefix', 'Match vocabulary by prefix', 'boolval', false)
+            ->option('-n --namespace-uri', 'Match vocabulary by namespace uri', 'boolval', false)
             ->option('-r --repository-id [repositoryId]', 'Filter by repository', 'strval')
 
             ->option('-u --update', 'Update existing vocabulary (if it exists)', 'boolval', false)
             ->usage(
-                '<bold>  $0 vocabulary:import-from-repo</end> <comment>schema</end><eol/>'
-                . '<bold>  $0 vocabulary:import-from-repo</end> <comment>"https://schema.org/"</end> <comment>--repository lov</end><eol/>'
-                . '<bold>  $0 vocabulary:import-from-repo</end> <comment>cidoc-crm</end> <comment>--version 7.1.3 --update</end><eol/>'
+                '<bold>  $0 vocabulary:import-from-repo</end> <comment>lov:event</end><eol/>'
+                . '<bold>  $0 vocabulary:import-from-repo</end> <comment>--prefix event</end><eol/>'
             );
     }
 
@@ -33,7 +32,9 @@ class ImportFromRepoCommand extends AbstractCommand
         $this->ensureOmekaInstance();
 
         // Init repository search arguments
-        $args = [];
+        $args = [
+            'type' => null,
+        ];
         if ($repositoryId) {
             $args['repositoryId'] = $repositoryId;
         }
@@ -58,7 +59,7 @@ class ImportFromRepoCommand extends AbstractCommand
         // Try to resolve vocabulary by different identifier types
         $vocabularyResult = $repositoryManager->find($identifier, ...$args);
         if (!$vocabularyResult) {
-            throw new InvalidArgumentException("Vocabulary '{$identifier}' not found.");
+            throw new InvalidArgumentException("No vocabulary found matching '{$identifier}'.");
         }
 
         $vocabularyItem = $vocabularyResult->getItem();
