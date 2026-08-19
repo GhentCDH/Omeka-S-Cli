80669aa56f4ac3e78938a2a2feb7d2abea968ac8
Frederic Lamsens <frederic.lamsens@ugent.be>
2026-08-19 12:41:06 +0200

    fix: check omeka.org module and theme api response structure

--- src/Repository/Theme/OmekaDotOrg.php
+++ src/Repository/Theme/OmekaDotOrg.php
@@ -33,8 +33,22 @@ class OmekaDotOrg extends AbstractRepository
         // Get the JSON data from the Omeka.org theme list
         $data = ResourceFetcher::fetchJson(self::API_ENDPOINT) ?? [];
 
+        // Check data structure
+        $firstItem = current($data);
+        foreach (['dirname', 'latest_version', 'versions', 'owner'] as $key) {
+            if (!array_key_exists($key, $firstItem)) {
+                throw new \UnexpectedValueException("Invalid data structure from " . self::API_ENDPOINT);
+            }
+        }
+
+
         // Create the modules array
         foreach ($data as $item) {
+            // an add-on without a published release can not be used
+            if (empty($item['latest_version']) || !isset($item['versions'][$item['latest_version']])) {
+                continue;
+            }
+
             $versions = [];
             foreach ($item['versions'] as $version => $versionInfo) {
                 $versions[$version] = new ThemeVersion(
