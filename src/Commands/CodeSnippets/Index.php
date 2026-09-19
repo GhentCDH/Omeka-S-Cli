819cafe6dcb9f2c458902b02866fa2b3745ddd69
erseco <info@ernesto.es>
2026-09-19 16:06:16 +0100

    chore: align CodeSnippets index with project conventions

--- src/Commands/CodeSnippets/Index.php
+++ src/Commands/CodeSnippets/Index.php
@@ -1,15 +1,4 @@
 <?php
-/**
- * Commands index for CodeSnippets.
- *
- * PHP version 8.1
- *
- * @category Command
- * @package  OSC\Commands\CodeSnippets
- * @author   Erseco <author@example.com>
- * @license  https://opensource.org/licenses/MIT MIT
- * @link     https://github.com/erseco/Omeka-S-Cli
- */
 
 namespace OSC\Commands\CodeSnippets;
 
